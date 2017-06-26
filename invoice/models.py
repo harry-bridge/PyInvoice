2bb00acc6a204cec84c32998b985fb857c5ad3d6
Harry Bridge <harry@harrybridge.co.uk>
2017-06-26 17:28:52 +0100

    Nicify list templates

--- invoice/models.py
+++ invoice/models.py
@@ -30,6 +30,12 @@ class Company(models.Model):
     address = models.TextField()
     email = models.CharField(max_length=150)
 
+    def total_invoices(self):
+        return self.invoice_set.all().count()
+
+    def latest_invoice(self):
+        return self.invoice_set.all().last()
+
     def __str__(self):
         return self.name
 
