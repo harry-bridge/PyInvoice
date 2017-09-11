118dff53746a481bfdbc2ba57d54bc0f9b420c6b
Harry Bridge <harrybridge01@gmail.com>
2017-09-11 23:57:40 +0100

    Add sentry config

--- PyInvoice/settings.py
+++ PyInvoice/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
+import raven
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -36,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'raven.contrib.django.raven_compat',
     'invoice',
 ]
 
@@ -77,6 +79,10 @@ WSGI_APPLICATION = 'PyInvoice.wsgi.application'
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
 if production:
+    RAVEN_CONFIG = {
+        'dsn': os.environ.get('PYINVOICE_RAVEN_DSN'),
+    }
+
     DATABASES = {
        'default': {
            'ENGINE': 'django.db.backends.postgresql_psycopg2',
