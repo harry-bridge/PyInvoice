961698480c300cff9562bd1aeaae108eee67aa59
Harry Bridge <harrybridge01@gmail.com>
2017-09-27 22:52:54 +0100

    Use dj-static to server static and media content

--- PyInvoice/settings.py
+++ PyInvoice/settings.py
@@ -135,14 +135,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
-
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = '/static/'
-
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-
-MEDIA_URL = '/media/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "invoice/static"),
     ]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = '/media/'
