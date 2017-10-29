7d41389e7e06417d16c865b4a629b78178cd98b6
Harry Bridge <harrybridge01@gmail.com>
2017-10-29 13:47:24 +0000

    Add person and phone number to company

--- invoice/models.py
+++ invoice/models.py
@@ -30,6 +30,8 @@ class Company(models.Model):
     name = models.CharField(max_length=200)
     address = models.TextField()
     email = models.CharField(max_length=150)
+    person = models.CharField(max_length=100, blank=True, null=True)
+    phone = models.CharField(max_length=20, blank=True, null=True)
 
     def __str__(self):
         return self.name
