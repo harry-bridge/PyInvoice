5237cc71955f3ce3040be0b9ca178da8a63531c2
Harry Bridge <harrybridge01@gmail.com>
2018-01-13 19:02:00 +0000

    Updated sample data creation

--- invoice/management/commands/createSampleData.py
+++ invoice/management/commands/createSampleData.py
@@ -30,13 +30,18 @@ class Command(BaseCommand):
         company_names = ["Acme, inc.","Widget Corp","123 Warehousing","Demo Company","Smith and Co.","Foo Bars","ABC Telecom","Fake Brothers","QWERTY Logistics","Demo, inc.","Sample Company","Sample, inc","Acme Corp","Allied Biscuit","Ankh-Sto Associates","Extensive Enterprise","Galaxy Corp","Globo-Chem","Mr. Sparkle","Globex Corporation","LexCorp","LuthorCorp","North Central Positronics","Omni Consimer Products","Praxis Corporation","Sombra Corporation","Sto Plains Holdings","Tessier-Ashpool","Wayne Enterprises","Wentworth Industries","ZiffCorp","Bluth Company","Strickland Propane","Thatherton Fuels","Three Waters","Water and Power","Western Gas & Electric","Mammoth Pictures","Mooby Corp","Gringotts","Thrift Bank","Flowers By Irene","The Legitimate Businessmens Club","Osato Chemicals","Transworld Consortium","Universal Export","United Fried Chicken","Virtucon","Kumatsu Motors","Keedsler Motors","Powell Motors","Industrial Automation","Sirius Cybernetics Corporation","U.S. Robotics and Mechanical Men","Colonial Movers","Corellian Engineering Corporation","Incom Corporation","General Products","Leeding Engines Ltd.","Blammo","Input, Inc.","Mainway Toys","Videlectrix","Zevo Toys","Ajax","Axis Chemical Co.","Barrytron","Carrys Candles","Cogswell Cogs","Spacely Sprockets","General Forge and Foundry","Duff Brewing Company","Dunder Mifflin","General Services Corporation","Monarch Playing Card Co.","Krustyco","Initech","Roboto Industries","Primatech","Sonky Rubber Goods","St. Anky Beer","Stay Puft Corporation","Vandelay Industries","Wernham Hogg","Gadgetron","Burleigh and Stronginthearm","BLAND Corporation","Nordyne Defense Dynamics","Petrox Oil Company","Roxxon","McMahon and Tate","Sixty Second Avenue","Charles Townsend Agency","Spade and Archer","Megadodo Publications","Rouster and Sideways","C.H. Lavatory and Sons","Globo Gym American Corp","The New Firm","SpringShield","Compuglobalhypermeganet","Data Systems","Gizmonic Institute","Initrode","Taggart Transcontinental","Atlantic Northern","Niagular","Plow King","Big Kahuna Burger","Big T Burgers and Fries","Chez Quis","Chotchkies","The Frying Dutchman","Klimpys","The Krusty Krab","Monks Diner","Milliways","Minuteman Cafe","Taco Grande","Tip Top Cafe","Moes Tavern","Central Perk","Chasers"]
 
         company_addresses = ['Somewhere in Birmingham', 'The Moon', 'My Mate\'s Garage']
-        company_emails = ['test@test.com']
+        company_emails = ['lmclean0@about.com','akalker1@telegraph.co.uk','adanbrook2@blog.com','rbream3@whitehouse.gov','hmaurice4@pen.io','eatty5@patch.com','afranz6@independent.co.uk','jbeazleigh7@illinois.edu','isaltsberger8@prlog.org','ajurisic9@cbc.ca']
+
+        person_names = ['Angelo Hearl','Celisse Lionel','Reid Poynor','Ervin Hamlen','Hamil Botha','Emery Laycock','Brandice Gaishson','Toma Kiloh','Marie-ann Conley','Teddie Ahern']
+        person_phone = ['8646257126','2217858480','5122970073','6439517819']
 
         for i in range(10):
             models.Company.objects.create(
-                name=company_names[i % len(company_names)],
+                name=random.choice(company_names),
                 address=company_addresses[i % len(company_addresses)],
-                email=company_emails[i % len(company_emails)]
+                email=company_emails[i % len(company_emails)],
+                person=random.choice(person_names),
+                phone=random.choice(person_phone)
                 )
 
     def create_invoices(self):
