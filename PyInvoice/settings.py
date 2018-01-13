c99a4a18fea95e61f6505c67a6e6136b6974e91d
Harry Bridge <harrybridge01@gmail.com>
2018-01-13 15:08:24 +0000

    Move testing db to postgres

--- PyInvoice/settings.py
+++ PyInvoice/settings.py
@@ -94,10 +94,21 @@ if production:
         }
       }
 else:
+    # DATABASES = {
+    #     'default': {
+    #         'ENGINE': 'django.db.backends.sqlite3',
+    #         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    #     }
+    # }
+
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'postgres',
+            'USER': 'postgres',
+            'PASSWORD': 'postgres',
+            'HOST': 'localhost',
+            'PORT': '5432',
         }
     }
 
